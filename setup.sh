ecc428c09128a3ca323555244d4e9d91bbcfbff2
sriramcu <csriram12345@gmail.com>
2024-12-05 17:16:21 +0530

    Started untracking obj files

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@ pip install gdown
 pip install -r requirements.txt
 pip install git+https://github.com/ozgur/python-firebase
 
-gdown '19EIcT9ei0QBGWSppdr0JgyK5vQv3GvtQ'
-gdown '1VXEucnpOw_uM-tczoQUO9VoLipvBeiCZ'
-gdown '1PTciOpFiJcHng-WwnUiENttvTbYjtdR1'
-gdown '1UK7U5wHRZwyD4b4zaV5_bbXJI6OtvBts'
+gdown 1TrWkNWCQVCCvN2iQQ_3QOSGEY4FToY1h
+gdown 1H5-Ci1wpJOyJC0T27dVLSZVk7jbrjoyu
+gdown 1O4ipOucBCBz_-NrMXHF4OlvPdoocj6AI
+gdown 1er0v8s6AQvkVBVjN9LippGqlR_6js4pV
 
 
 mv *.weights weights/
